842a0da1116a0a4d1fc2d5a5de11d2f7f727f24e
Mikadows <Mikadows@DESKTOP-K1DQJ1M>
2018-12-14 12:34:35 +0100

    Mise en place du fichier properties

--- ProAgenda/src/fr/proagenda/classes/PropertyAcces.java
+++ ProAgenda/src/fr/proagenda/classes/PropertyAcces.java
@@ -1,16 +1,59 @@
 package fr.proagenda.classes;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class PropertyAcces {
-	private String Address;
+	private String dbAddress;
 	private String dbLogin;
 	private String dbPswd;
+	private String dbPort;
 	
-	public String getAddress() {
-		return Address;
+	public PropertyAcces() {
+		super();
+		Properties prop = new Properties();
+		InputStream input = null;
+
+		try {
+
+			input = new FileInputStream("config.properties");
+
+			// load a properties file
+			prop.load(input);
+
+			// get the property value and print it out
+			this.dbAddress = prop.getProperty("dbAddress");
+			this.dbLogin = prop.getProperty("dbLogin");
+			this.dbPswd = prop.getProperty("dbPassword");
+			this.dbPort = prop.getProperty("dbPort");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	  }
+	
+	public String getDbAddress() {
+		return dbAddress;
 	}
+
+	public String getDbPort() {
+		return dbPort;
+	}
+
 	public String getDbLogin() {
 		return dbLogin;
 	}
+	
 	public String getDbPswd() {
 		return dbPswd;
 	}
