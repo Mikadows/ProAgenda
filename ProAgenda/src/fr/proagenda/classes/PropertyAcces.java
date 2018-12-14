1ce7cc168b5a0a8e2aacdfd2f58f02773db60820
Mikadows <Mikadows@DESKTOP-K1DQJ1M>
2018-12-14 12:08:55 +0100

    getters

--- ProAgenda/src/fr/proagenda/classes/PropertyAcces.java
+++ ProAgenda/src/fr/proagenda/classes/PropertyAcces.java
@@ -4,4 +4,15 @@ public class PropertyAcces {
 	private String Address;
 	private String dbLogin;
 	private String dbPswd;
+	
+	public String getAddress() {
+		return Address;
+	}
+	public String getDbLogin() {
+		return dbLogin;
+	}
+	public String getDbPswd() {
+		return dbPswd;
+	}
+	
 }
